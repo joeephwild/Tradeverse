2d36a3ec67f9a7364ee1a09df86d89005e789dce
hypotyposis <3160101760@zju.edu.cn>
2023-06-19 16:58:12 +0800

    chore: add contractCall

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,70 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Button, MainNav } from "@/components";
+import { useTradeContext } from "@/context";
+import { useEffect } from "react";
+import { WALLET } from "@dataverse/runtime-connector";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { runtimeConnector } = useTradeContext();
+
+  useEffect(() => {
+    console.log({ runtimeConnector });
+  }, []);
+
+  const contractCall = async () => {
+    await runtimeConnector?.connectWallet(WALLET.METAMASK);
+    const res = await runtimeConnector?.contractCall({
+      contractAddress: "0xB07E79bB859ad18a8CbE6E111f4ad0Cca2FD3Da8",
+      abi: [
+        {
+          inputs: [],
+          name: "metadata",
+          outputs: [
+            {
+              components: [
+                {
+                  internalType: "address",
+                  name: "hub",
+                  type: "address",
+                },
+                {
+                  internalType: "uint256",
+                  name: "profileId",
+                  type: "uint256",
+                },
+                {
+                  internalType: "uint256",
+                  name: "pubId",
+                  type: "uint256",
+                },
+                {
+                  internalType: "address",
+                  name: "collectModule",
+                  type: "address",
+                },
+              ],
+              internalType: "struct IDataToken.Metadata",
+              name: "",
+              type: "tuple",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+      ],
+      method: "metadata",
+      params: [],
+    });
+    console.log({ res });
+  };
+
   return (
     <main className="bg-hero-pattern object-cover  bg-no-repeat !w-screen h-[880px] h-creen">
       <MainNav />
-      <div >
+      <div>
         <h1>
           Future of E-commerce: <br />
           Connect, Engage, and Transact
@@ -20,6 +76,7 @@ export default function Home() {
           sessions.
         </p>
         <Button title="Get Started" />
+        <button onClick={contractCall}>contractCall</button>
       </div>
     </main>
   );
